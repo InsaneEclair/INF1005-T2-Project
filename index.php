7948e29c6866b5048f30ed50ad52098943093e24
Yao Hao <2302952@sit.singaporetech.edu.sg>
2024-03-26 22:31:14 +0800

    Added Code and Remove unnecessary images

--- index.php
+++ index.php
@@ -16,92 +16,150 @@
             include "inc/header.inc.php";
         ?>
         <main class="container">
-            <section id="dogs">
-                <h2>All About Dogs!</h2>
+            <section id="fish">
+                <h2>Fishes in Store!</h2>
                 <div class="row">
                 <article class="col-sm">
-                    <h3>Poodles</h3>
+                    <h3>Albino 'Lucy' Stingray</h3>
                     <figure>
-                        <img src="images/Albino 'Lucy' Stingray.jpg" alt="als" class="img-thumbnail"
+                        <img src="images/Albino 'Lucy' Stingray.jpeg" alt="lucyray" class="img-thumbnail"
                         title="View larger image..."/>
                         </a>
-                        <figcaption>Albino 'Lucy' Stingray</figcaption>
+                        <figcaption>Albino Stingray</figcaption>
                     </figure>
                     <p>
-                    Albino Lucy Stingray are a group of formal dog breeds, the Standard
-                    Poodle, Miniature Poodle and Toy Poodle...
+                    Albino Lucy 'Stingray' is a hybrid stingray from Pearl and Black Diamond Stingray with albinism, 
+                    result of heritable mutations. 
                     </p>
                 </article>
                 <article class="col-sm">
-                    <h3>Chihuahua</h3>
+                    <h3>Black Diamond 'Batman' Stingray</h3>
                     <figure>
-                        <img src="images/chihuahua_small.jpg" alt="Chihuahua" class='img-thumbnail'
+                        <img src="images/Black Diamond 'Batman' Stingray.jpeg" alt="bdray" class='img-thumbnail'
                         title="View larger image..."/>
                         </a>
-                        <figcaption>Chihuahua</figcaption>
+                        <figcaption>BD Stingray</figcaption>
                     </figure>
                     <p>
-                        The Chihuahua is the smallest breed of dog, and is named
-                        after the Mexican state of Chihuahua...
+                    The diamond stingray is a species of stingray in the family Dasyatidae. It is found in the 
+                    coastal waters of the eastern Pacific Ocean from southern California to northern 
+                    Chile, and around the Galápagos and Hawaiian Islands
                     </p>
                 </article>
                 <article class="col-sm">
-                    <h3>Mongrel</h3>
+                    <h3>Albino Red Eared Slider</h3>
                     <figure>
-                        <img src="images/mydog_small.jpg" alt="My Dog" class = 'img-thumbnail'
+                        <img src="images/Albino Red Eared Slider.jpeg" alt="AlbinoRES" class = 'img-thumbnail'
                         title="View larger image..."/>
                         </a>
-                        <figcaption>My Dog</figcaption>
+                        <figcaption>Albino RES</figcaption>
                     </figure>
                     <p>
-                        This is my own dog, she was a very naughty dog,
-                        she passed in 2022, I still miss her.
+                    Albino Red Eared Sliders (Trachemys scripta elegans) are one of the most striking of all turtles to keep as pets, their survival in the wild is almost impossible.
+                    Luckily a few breeders, produce them for keepers to appreciate and enjoy. Albino Red Ears behave the same as normal Red Eared Sliders. 
+                    All our turtles are healthy, active and feeding on commercial pellets.
                     </p>
+                </article>
+                <article class="col-sm">
+                    <h3>Caramel Pink Red Eared Slider</h3>
+                    <figure>
+                        <img src="images/Caramel Red Eared Slider.jpeg" alt="CaramelRES" class = 'img-thumbnail'
+                        title="View larger image..."/>
+                        </a>
+                        <figcaption>Caramel RES</figcaption>
+                    </figure>
+                    <p>
+                    Caramel Pink Albino Red Eared Sliders are the result of nearly 20 years of very patient selective breeding for very beautiful color traits in Albino Red Eared Sliders.
+                    From pink to lavender skin highlights, eyes with deep blue iris' and red pupils, to creamy white and golden tan shells, 
+                    there are no other turtles in the world even close to these unique creatures
+                    </p>
+                </article>
+                <article class="col-sm">
+                    <h3>Altum Angelfish</h3>
+                    <figure>
+                        <img src="images/Altums.jpg" alt="Altums" class = 'img-thumbnail'
+                        title="View larger image..."/>
+                        </a>
+                        <figcaption>Altums</figcaption>
+                    </figure>
+                    <p>
+                    Pterophyllum altum, also referred to as the altum angelfish, deep angelfish, or Orinoco angelfish,
+                    occurs strictly in the Orinoco River Basin and the Upper Rio Negro watershed in Southern Venezuela, 
+                    Southeastern Colombia and extreme Northern Brazil.
+                    </p>
+                </article>
+                <article class="col-sm">
+                    <h3>Betta Fighting Fish</h3>
+                    <figure>
+                        <img src="images/Betta.jpg" alt="Betta" class = 'img-thumbnail'
+                        title="View larger image..."/>
+                        </a>
+                        <figcaption>Betta</figcaption>
+                    </figure>
+                    <p>
+                    The Siamese fighting fish (Betta splendens), commonly known as the betta,
+                    is a freshwater fish native to Southeast Asia, namely Cambodia, Laos, Myanmar, Malaysia, 
+                    Indonesia, Thailand, and Vietnam
+                    </p>
+                </article>
+                <article class="col-sm">
+                    <h3>Blue Eyed Pleco</h3>
+                    <figure>
+                        <img src="images/BEC.jpg" alt="BEC" class = 'img-thumbnail'
+                        title="View larger image..."/>
+                        </a>
+                        <figcaption>BEC</figcaption>
+                    </figure>
+                    <p>
+                    Long considered a “holy grail” fish in the aquarium hobby, the Blue Eyed Pleco or Blue Eye Panaque remains 
+                    a showy and impressive fish despite becoming more readily available in recent years. Like many Panaque 
+                    species, it is found in fast moving waters – specifically, the Rio Magdalena in Colombia
+                    </p>
+                </article>
+                <article class="col-sm">
+                    <h3>L236 Super White</h3>
+                    <figure>
+                        <img src="images/Hypancistrus L236.jpeg" alt="L236" class = 'img-thumbnail'
+                        title="View larger image..."/>
+                        </a>
+                        <figcaption>L236</figcaption>
+                    </figure>
+                    <p>
+                    This very popular pleco is found in a lot of tanks of pleco enthusiasts, liked for its bright white coloration.
+                    As one of the few line-bred plecos, also "L236" was bred and selected over generations. The most famous breeding line is from Robert Budrovcan, where the offspring are labeled as "RB".
+                    </p>
+                </article>
+                <article class="col-sm">
+                    <h3>Pseudacanthicus pitanga L24</h3>
+                    <figure>
+                        <img src="images/L24.jpg" alt="L24" class = 'img-thumbnail'
+                        title="View larger image..."/>
+                        </a>
+                        <figcaption>L24</figcaption>
+                    </figure>
+                    <p>
+                    Pseudacanthicus pitanga "L24" is a very large growing pleco. It can reach sizes of over 40 cm. 
+                    Most specimens in captivity stay around 30 - 35 cm body length. The whole body is covered with hard spikes, 
+                    which can cause bleeding wounds when touched with the bare hands. 
+                    This fish shows big fins and an overall strong body.
+                    </p>
+                </article>
+                <article class="col-sm">
+                    <h3>Shortbody Super Red Arowana</h3>
+                    <figure>
+                        <img src="images/Short Body Super Red Arowana.jpeg" alt="L236" class = 'img-thumbnail'
+                        title="View larger image..."/>
+                        </a>
+                        <figcaption>SR Arowana</figcaption>
+                    </figure>
+                    <p>
+                    The Super Red Arowana is a large freshwater fish that can grow up to 90 cm (35 inches) in length. They have a long, streamlined body with large scales and a distinctive barbels on their lower jaws. 
+                    The fish has a bold, vibrant red coloration throughout its body with a hint of orange or gold. Their fins are typically red, with some specimens having a hint of gold or black. 
+                    Juvenile Super Red Arowanas have a darker coloration, but they become brighter as they mature.  Super Red Arowanas are known for their aggressive behavior and territoriality, particularly towards members of their own species.    
+                    The red, super red, blood red, or chili red is known only from the upper part of the Kapuas River and nearby lakes in western Indonesian part of Borneo.
+                    </p>
+                </article>
                 </div>
-            </section>
-            <section id = "cats">
-                <h2>All About Cats!</h2>
-                <div class="row">
-                <article class="col-sm">
-                    <h3>Tabby</h3>
-                    <figure>
-                        <img src="images/tabby_small.jpg" alt="Tabby" class="img-thumbnail"
-                        title="View larger image..."/>
-                        </a>
-                        <figcaption>Tabby Cat</figcaption>
-                    </figure>
-                    <p>
-                        A tabby is any domestic cat that has a coat featuring
-                        distinctive stripes, dots, lines or swirling patterns,
-                        usually together with a mark resembling an 'M' on its
-                        forehead...
-                    </p>
-                    <figure>
-                        <img src="images/mycat_small.jpg" alt="My Cat" class='img-thumbnail'
-                        title="View larger image..."/>
-                        </a>
-                        <figcaption>My Cat</figcaption>
-                    </figure>
-                    <p>
-                        This is my own cat, she is a very cute orange tabby,
-                        She was a stray for the majority of her life. 
-                    </p>
-                </article>
-                <article class="col-sm">
-                    <h3>Calico</h3>
-                    <figure>
-                        <img src="images/calico_small.jpg" alt="Calico" class="img-thumbnail"
-                        title="View larger image..."/>
-                        </a>
-                        <figcaption>Calico Cat</figcaption>
-                    </figure>
-                    <p>
-                        Calico cats are domestic cats with a spotted or parti-
-                        colored coat that is predominantly white, with patches
-                        of two other colors...
-                    </p>
-                </article>
-            </div>
             </section>
         </main>   
         <?php
